19ed15ddcb11056e3919022f969c7a6069fe387d
Josue-Santos-Pinto <js.pinto55@gmail.com>
2023-01-05 11:08:45 -0300

    Add Firebase and FirebaseAuth

--- src/Screens/Preload/index.js
+++ src/Screens/Preload/index.js
@@ -1,10 +1,78 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import auth from '@react-native-firebase/auth';
+import AnimatedLottieView from "lottie-react-native";
+import { useNavigation } from "@react-navigation/native";
+
+export default () => {
+    // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  const navigation = useNavigation()
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    
+        const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+        return subscriber; // unsubscribe on unmount
+    
+    
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    setTimeout(()=>{
+        navigation.reset({index:1,routes:[{name:'Login'}]})
+    },2000)
+     
+  } else {
+    setTimeout(()=>{
+        navigation.reset({index:1,routes:[{name:'Home'}]})
+    },2000)
+    
+  }
+    
+
+    return (
+        <View className="flex-1 items-center justify-center bg-slate-800 ">
+            <AnimatedLottieView 
+                source={require('../../assets/lottie/electric-guitar-music.json')}
+                autoPlay={true}
+                loop={true}
+                resizeMode='contain'
+            />
+        </View>
+    )
+}
+
+
+  
+
+  
+
+  
+
+ 
+
+ 
+
+ 
+   
+
+/*import React, { useEffect } from "react";
 import { ActivityIndicator, Text, View } from "react-native";
 import AnimatedLottieView from "lottie-react-native";
 import { useNavigation } from "@react-navigation/native";
 
 export default () => {
-    const navigation = useNavigation()
+    
 
     useEffect(()=>{
         setTimeout(()=>{
@@ -27,3 +95,4 @@ export default () => {
         </View>
     )
 }
+*/
