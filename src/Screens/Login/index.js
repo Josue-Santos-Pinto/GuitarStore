27f400a27dfb12a3fc480417007e777f374a55c1
Josue-Santos-Pinto <js.pinto55@gmail.com>
2023-03-31 16:06:41 -0300

    fix conflict

--- src/Screens/Login/index.js
+++ src/Screens/Login/index.js
@@ -26,7 +26,7 @@ export default () => {
 
   const handleSignIn = () => {
     setIsLoading(true);
-    if (emailLogin.trim().length > 6 && password.trim().length > 5) {
+    if (emailLogin.trim().length > 6 && password.trim().length > 6) {
       auth()
         .signInWithEmailAndPassword(emailLogin, password)
         .then(() => {
